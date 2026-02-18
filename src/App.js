7e53854abc64bb56936178050da65c6dab6b2369
Dawid Gorlicki <dcisek434@gmail.com>
2026-02-18 12:29:08 +0100

    Komponent news

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import{News} from "./components/news";
 
 function App() {
+
+const name = "Kuba";
+
   return (
-    <div className="App">
+    <div className="crazy-glow">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello {name}!</h1>
+        <News></News>
       </header>
     </div>
   );
